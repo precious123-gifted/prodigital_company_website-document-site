592c8c5a322aa89a9c1b76d307d7eba7d6660027
precious123-gifted <ogunameprecious123@gmail.com>
2024-09-24 01:12:09 +0100

    my first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Prodigital Company Website Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/precious123-gifted',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.linkedin.com/in/oguname-precious-51800020a',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+ 
+ 
 }
 
 export default config
